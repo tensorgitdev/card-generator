b26d086dac5f9621e95d73bcba5071c5a575c0ef
tensorgitdev <tensorggacc@gmail.com>
2026-06-28 03:47:38 +0900

    init: card generator

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ const SUPABASE_URL = 'https://mqruxlhrxniyzbhkhmtc.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xcnV4bGhyeG5peXpiaGtobXRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzMjgzMDIsImV4cCI6MjA4MzkwNDMwMn0.qPt-dN4Uj0d0pKU11AYy782XMuoXeJ7CFiVXmEyrJzA'
 
 // Cloudflare Worker URL
-const WORKER_URL = 'https://YOUR_WORKER.YOUR_SUBDOMAIN.workers.dev'
+const WORKER_URL = 'https://card-generator.tensorggacc.workers.dev'
